405e692321d395067150c40d35ebdc68530c13f3
Hiep Trinh <hieptq.social@gmail.com>
2020-11-12 00:39:21 +1100

    feat: pointing to live aws microservice

--- src/services/makeRequest.ts
+++ src/services/makeRequest.ts
@@ -1,58 +1,12 @@
 import axios from 'axios';
 
-const API_KEY = 'WBBcwnwQpV89';
-// interface getDataParams {
-//   searchValue: string;
-//   languageTarget?: string;
-// }
-
-exports.getSentences = async (args) => {
-  const fullUrl = `https://api.tracau.vn/${API_KEY}/s/${encodeURI(args.searchValue)}/${args.languageTarget}`;
-
-  const headers = {
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-    'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
-    'Access-Control-Allow-Origin': '*',
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Credentials': 'true',
-    'User-Agent':
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/600.7.12 (KHTML, like Gecko) Version/8.0.7 Safari/600.7.12',
-  };
-  try {
-    const result = await axios.get(fullUrl, { headers });
-    return result;
-  } catch (err) {
-    console.log(err);
-    return;
-  }
-};
-
 export interface getDataParams {
   searchValue: string;
   languageTarget?: string;
 }
 
 export const getSentences = async (args: getDataParams) => {
-  /* const fullUrl = `http://localhost:3001/`;
-  return await axios.get(fullUrl, { params: args }); */
-  const fullUrl = `https://api.tracau.vn/${API_KEY}/s/${encodeURI(args.searchValue)}/${args.languageTarget}`;
-
-  const headers = {
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-    'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
-    'Access-Control-Allow-Origin': '*',
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Credentials': 'true',
-    'User-Agent':
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/600.7.12 (KHTML, like Gecko) Version/8.0.7 Safari/600.7.12',
-  };
-  try {
-    const result = await axios.get(fullUrl, { headers });
-    return result;
-  } catch (err) {
-    console.log(err);
-    return;
-  }
+  // http://localhost:3001/
+  const fullUrl = `https://nk3mrgnqu9.execute-api.us-east-1.amazonaws.com/dev/`;
+  return await axios.get(fullUrl, { params: args });
 };
