8fec9bc6a39ff122a8897a04693eedaa58e6366e
Hiep Trinh <hieptq.social@gmail.com>
2020-12-23 18:38:35 +1100

    remove unnecessary code

--- src/index-foreground.jsx
+++ src/index-foreground.jsx
@@ -2,7 +2,5 @@ import * as React from 'react';
 
 import { render } from 'react-dom';
 
-render(
-  <div style={{ backgroundColor: 'red', width: '700px', hight: '600px' }}>Foreground component</div>,
-  document.querySelector('#foreground')
-);
+// can inject component here
+render(<div id="foreground-app" />, document.querySelector('#foreground'));
